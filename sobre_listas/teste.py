af23a2b482e63132154a1266c3d35e74a4884660
Pedro Manoel <pedromanoelpyjs@gmail.com>
2025-09-09 23:01:56 -0300

    .

--- sobre_listas/teste.py
+++ sobre_listas/teste.py
@@ -1,18 +1,21 @@
-from typing import Dict, List
+# Faça um programa que peça as quatro notas de 10 alunos, calcule e armazene num vetor a média de cada aluno,
+# imprima o número de alunos com média maior ou igual a 7.0.
 
-def coleta_dados_alunos(quantidade_alunos: int, quantidade_notas: int) -> List[Dict]:
+def coleta_dados_alunos(quantidade_alunos, quantidade_notas):
 
     alunos = []
 
     for i in range(1, quantidade_alunos + 1):
 
-        nome = input(f'Insira o nome do {i} aluno: ')
+        while True:
 
-        if nome.strip() and not nome.isdigit():
+            nome = input(f'Insira o nome do {i} aluno: ')
 
-            break
+            if nome.strip() and not nome.isdigit():
 
-        print('\033[mPor favor insira um nome válido!\033[m')
+                break
+
+            print('\033[1;31mPor favor insira um nome válido!\033[m')
 
         notas_validas = []
 
@@ -22,7 +25,7 @@ def coleta_dados_alunos(quantidade_alunos: int, quantidade_notas: int) -> List[D
 
                 try:
                         
-                    nota = float(input(f'Insira a {j+1}ª nota de {nome}: '))
+                    nota = float(input(f'Insira a {j}ª nota de {nome}: '))
                             
                     if 0 <= nota <= 10:
 
@@ -36,8 +39,52 @@ def coleta_dados_alunos(quantidade_alunos: int, quantidade_notas: int) -> List[D
                 except ValueError:
 
                     print('\033[1;31mPor favor, coloque um valor numérico válido.\033[m')
+    
+        aluno_info = {'nome': nome, 'notas': notas_validas}
+        alunos.append(aluno_info)
+
+    return alunos
 
 
 
+def calcular_media(notas):
 
-            
+    media = 0
+
+    for nota in notas:
+
+        media += nota
+
+    media = media / QUANTIDADE_NOTAS
+    return media
+
+
+def exibir_dados(dados):
+
+    aprovados = 0
+    LARGURA = 30
+    TITULO = 'Resultado'
+    print('-' * LARGURA)
+    print(TITULO.center(30))
+    print('-' * LARGURA)
+
+    for aluno in dados:
+
+        media = calcular_media(aluno['notas'])
+        print(f'O aluno {aluno['nome']} teve as seguintes notas: {aluno['notas']} e ficou com a média de {media:.2f}')
+
+        if media >= 7:
+
+            aprovados += 1
+    
+    print(f'{aprovados} alunos aprovados')
+
+
+
+if __name__ == '__main__':
+
+    QUANTIDADE_ALUNOS = 10
+    QUANTIDADE_NOTAS = 4
+    lista_alunos = coleta_dados_alunos(QUANTIDADE_ALUNOS, QUANTIDADE_NOTAS)
+    exibir_dados(lista_alunos)
+          
