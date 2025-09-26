28f688fdaca5441663def55d797c94afa6ba2607
Pedro Manoel <pedromanoelpyjs@gmail.com>
2025-09-26 17:10:37 -0300

    .

--- sobre_listas/ex_18.py
+++ sobre_listas/ex_18.py
@@ -40,3 +40,52 @@ Jogador         Votos           %
 11              1               12,5%
 O melhor jogador foi o número 9, com 4 votos, correspondendo a 50% do total de votos.
 '''
+def main():
+    total_votos = 0
+    jogadores = []
+
+    while True:
+
+        try:
+            voto = input('Digite qual jogador foi melhor: ')
+            voto_int = int(voto)
+        
+            if voto_int == 0:
+                break
+
+            elif voto_int > 23 or voto_int < 1:
+                print('\033[1;31mPor favor coloque um número entre 1 a 23\033[m')
+
+            else:
+                jogadores.append(voto_int)
+                total_votos +=1
+        except ValueError:
+            print('\033[1;31mPor favor coloque um número válido!\033[m')
+
+
+    jogadores_nao_repetidos = set(jogadores)
+    linhas_saida = ['\nResultados das votações:\n']
+    linhas_saida.append('Foram computados {total_votos} votos.\n')
+    linhas_saida.append(f'{'Jogador':>5}{'votos':>15}{'%':>16}')
+    porcentagens = []
+    for jogador in jogadores_nao_repetidos:
+        total_jogador = jogadores.count(jogador)
+        porcentagem_jogador = (total_jogador/total_votos) * 100
+        linhas_saida.append(f'{jogador:>5}{total_jogador:>15}{porcentagem_jogador:>20.1f} %\n')
+        porcentagens.append(round(porcentagem_jogador, 1))
+
+    indice = porcentagens.index(max(porcentagens))
+    linhas_saida.append(f'O melhor jogador foi o camisa {indice +1}, com {max(porcentagens)}%')
+
+    saida_formatada = "\n".join(linhas_saida)
+    print(saida_formatada)
+    try:
+        nome_arquivo = 'C:\\Users\\Pedro\\Documents\\GitHub\\lista_de_exercicios\\Lista_de_exercicios\\sobre_listas\\resultado_enquete.txt'
+        with open(nome_arquivo, 'w', encoding='utf-8') as f:
+            f.write(saida_formatada)
+        print(f"\nDados salvos com sucesso no arquivo '{nome_arquivo}'.")
+    except IOError as e:
+        print(f"\nErro ao salvar o arquivo: {e}")
+
+if __name__ == "__main__":
+    main()
