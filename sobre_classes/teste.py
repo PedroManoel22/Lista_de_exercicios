b533aad748d4661658c48a654c6354e9f9115244
Pedro Manoel <pedromanoelpyjs@gmail.com>
2025-11-19 16:52:04 -0300

    .

--- sobre_classes/teste.py
+++ sobre_classes/teste.py
@@ -1,10 +1,21 @@
-import re
+# import re
 
-padrao_num_conta = re.compile(r'\d{8}-\d{2}')
-x = input('Coloque o numero da contya: ')
-valido = padrao_num_conta.findall(x)
-if valido:
-    print('válido')
-else:
-    print('inválido')
-print(valido)
+# padrao_num_conta = re.compile(r'\d{8}-\d{2}')
+# x = input('Coloque o numero da contya: ')
+# valido = padrao_num_conta.findall(x)
+# if valido:
+#     print('válido')
+# else:
+#     print('inválido')
+# print(valido)
+
+from pprint import pprint
+data = [
+    {"id":1, "nome": "Maria", "salario": 4500.03},
+    {"id":2, "nome": "José", "salario": 6500.05},
+    {"id":3, "nome": "Antonio", "salario": 3409.98},
+    {"id":4, "nome": "Ana", "salario": 5093.34},
+    {"id":5, "nome": "Mariana", "salario": 3458.54},
+    {"id":6, "nome": "Ana", "salario": 10932.59},
+]
+pprint(data)
