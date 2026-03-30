8f3ed203445c23321adbff00c4b8768ab4fe74d9
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-03-30 17:21:44 -0300

    Update ex_09.py

--- sobre_funcoes/ex_09.py
+++ sobre_funcoes/ex_09.py
@@ -1,7 +1,7 @@
 # Reverso do número. Faça uma função que retorne o reverso de um número inteiro informado. Por exemplo: 127 -> 721.
 
 # obtém apenas números int
-def obter_inteiro_valido(mensagem='Insira um número inteiro: '):
+def obter_inteiro_valido(mensagem: str ='Insira um número inteiro: ') -> None | int:
     while True:
         try:
             num = int(input(mensagem))
@@ -11,12 +11,12 @@ def obter_inteiro_valido(mensagem='Insira um número inteiro: '):
             print("❌ \033[1;31mEntrada inválida. Por favor, digite um número inteiro.\033[m")
             
 # retorna o inverso do número inserido
-def reverso_numero(num):
+def reverso_numero(num: int) -> str:
     num_str = str(num)
     return f'número {num} ao contrário fica: {num_str[::-1]}'
 
 
 if __name__ == '__main__':
     num = obter_inteiro_valido()
-    num_reverso = reverso_numero(num)
-    print(num_reverso)
+    if num is not None:
+        print(reverso_numero(num))
