ecc3402c791f8a94bc3204c4024e66b57fc24343
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-04-01 19:06:40 -0300

    Implementando Type Hints ex_11.py

--- sobre_listas/ex_11.py
+++ sobre_listas/ex_11.py
@@ -1,10 +1,10 @@
 # Altere o programa anterior, intercalando 3 vetores de 10 elementos cada.
 
-vetor_1 = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
-vetor_2 = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
-vetor_3 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+vetor_1: list[int] = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+vetor_2: list[int] = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+vetor_3: list[str] = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
 
-vetor_4 = []
+vetor_4: list[int | str] = []
 
 for i in range(len(vetor_1)):
 
