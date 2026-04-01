10725ccd156759a7d91b5a684751f271169b55ba
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-04-01 19:06:33 -0300

    Implementando Type Hints ex_10.py

--- sobre_listas/ex_10.py
+++ sobre_listas/ex_10.py
@@ -1,10 +1,10 @@
 # Faça um programa que leia dois vetores com 10 elementos cada. Gere um terceiro vetor de 20 elementos,
 # cujos valores deverão ser compostos pelos elementos intercalados dos dois outros vetores.
 
-vetor_1 = [1, 3, 5, 7, 9]
-vetor_2 = [2, 4, 6, 8, 10]
+vetor_1: list[int] = [1, 3, 5, 7, 9]
+vetor_2: list[int] = [2, 4, 6, 8, 10]
 
-vetor_3 = []
+vetor_3: list[int] = []
 
 for i in range(len(vetor_1)):
 
