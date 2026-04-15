842fae916bb2c351be82bbd0e577fc660a8dea1d
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-04-15 17:35:10 -0300

    Create data.py

---
+++ lista_de_dados/data.py
@@ -0,0 +1,8 @@
+data: list[dict[str, str |float]] = [
+    {"id":1, "nome": "Maria", "salario": 4500.03},
+    {"id":2, "nome": "José", "salario": 6500.05},
+    {"id":3, "nome": "Antonio", "salario": 3409.98},
+    {"id":4, "nome": "Ana", "salario": 5093.34},
+    {"id":5, "nome": "Mariana", "salario": 3458.54},
+    {"id":6, "nome": "Ana", "salario": 10932.59},
+]
