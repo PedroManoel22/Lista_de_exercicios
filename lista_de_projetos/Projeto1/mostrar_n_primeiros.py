688cffca8dbc04c0da9ca2ed653d6edc6828f028
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-05-24 18:22:23 -0300

    Implementando Type Hints

--- lista_de_projetos/Projeto1/mostrar_n_primeiros.py
+++ lista_de_projetos/Projeto1/mostrar_n_primeiros.py
@@ -1,9 +1,11 @@
 from funcoes import ler_arquivo, mostrar_n_primeiros
 
+
 def main():
     arquivo = "Lista_de_exercicios/lista_de_projetos/Projeto1/relatorio.txt"
     dados = ler_arquivo(arquivo)
     mostrar_n_primeiros(dados)
+
 
 if __name__ == "__main__":
     main()
