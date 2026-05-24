0f62e5b14d8d1d273c31c0797f82e57bf7caa141
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-05-24 18:22:47 -0300

    Implementando Type Hints

--- lista_de_projetos/Projeto1/ordena_pelo_espaco.py
+++ lista_de_projetos/Projeto1/ordena_pelo_espaco.py
@@ -1,10 +1,12 @@
 from funcoes import ler_arquivo, ordenar
 
-# orndena o relatório.txt pelo estpaço ocupado
+
+# ordena o relatório.txt pelo estpaço ocupado
 def main():
     nome_arquivo = "Lista_de_exercicios/lista_de_projetos/Projeto1/relatorio.txt"
     dados = ler_arquivo(nome_arquivo)
     ordenar(dados)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
