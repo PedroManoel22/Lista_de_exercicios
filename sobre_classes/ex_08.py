72e83138f70c61a9904c73da3ce40cb44a3e40c3
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-05-25 12:46:07 -0300

    Implementando Type Hints

--- sobre_classes/ex_08.py
+++ sobre_classes/ex_08.py
@@ -1,119 +1,120 @@
 # Desenvolva uma classe Macaco, que possua os atributos nome e bucho (estomago) e pelo menos os métodos comer(), ver_bucho() e digerir().
-# Faça um programa ou teste interativamente, criando pelo menos dois macacos, alimentando-os com pelo menos 3 alimentos diferentes e verificando 
+# Faça um programa ou teste interativamente, criando pelo menos dois macacos, alimentando-os com pelo menos 3 alimentos diferentes e verificando
 # o conteúdo do estomago a cada refeição.
 class Macaco:
-    def __init__ (self, nome: str , bucho: list[str] = []):
+    def __init__(self, nome: str, bucho: list[str] = []):
         self.nome = nome
         self.bucho = bucho
-    
 
-  
     def digerir(self) -> None:
         while True:
-            alimento = input(f'\nQual alimento o macaco {self.nome} irá digerir? ')
+            alimento = input(f"\nQual alimento o macaco {self.nome} irá digerir? ")
             if alimento not in self.bucho:
-                print('\n\033[1;31mPor favor coloque um alimento que esteja no bucho do macaco\033[m')
-            
+                print(
+                    "\n\033[1;31mPor favor coloque um alimento que esteja no bucho do macaco\033[m"
+                )
+
             else:
                 self.bucho.remove(alimento)
-                print(f'\n\033[1;32m{alimento} digerido com sucesso!\033[m')
+                print(f"\n\033[1;32m{alimento} digerido com sucesso!\033[m")
                 break
-            
-
 
     def ver_bucho(self) -> None:
         if not self.bucho:
-            print(f'\nO bucho do macaco {self.nome} está vazio!\n')
-        
+            print(f"\nO bucho do macaco {self.nome} está vazio!\n")
+
         else:
-            print(f'\nNo bucho do macaco {self.nome} tem:\n')
+            print(f"\nNo bucho do macaco {self.nome} tem:\n")
             for comida in self.bucho:
                 print(comida)
             print()
 
-    
     def comer(self) -> None:
-        alimento = input('Qual alimento o macaco vai comer? ')
+        alimento = input("Qual alimento o macaco vai comer? ")
         self.bucho.append(alimento)
-        print(f'\n\033[1;32mMacaco {self.nome} comeu {alimento}\033[m')
+        print(f"\n\033[1;32mMacaco {self.nome} comeu {alimento}\033[m")
         self.ver_bucho()
-
-    
-
 
 
 def pede_nome() -> str:
     while True:
-        nome = input('Insira o nome do macaco: ')
+        nome = input("Insira o nome do macaco: ")
 
         if nome.isdigit():
-            print('\n\033[1;31mO nome do macaco não pode ser apenas números\033[m\n')
-        
+            print("\n\033[1;31mO nome do macaco não pode ser apenas números\033[m\n")
+
         elif len(nome) <= 2:
-            print('\n\033[1;31mO nome do macaco tem que ter pelo menos 2 caracteres!\033[m\n')
-        
+            print(
+                "\n\033[1;31mO nome do macaco tem que ter pelo menos 2 caracteres!\033[m\n"
+            )
+
         else:
             return nome
 
-def pede_bucho(nome: str) -> list[str]:
-    alimentos: list[str]  = []
-    while True:
-        comeu = input(f'O macaco chamado {nome} comeu alguma coisa (S/N)? ').upper().strip()
 
-        if comeu == 'S':
+def pede_bucho(nome: str) -> list[str]:
+    alimentos: list[str] = []
+    while True:
+        comeu = (
+            input(f"O macaco chamado {nome} comeu alguma coisa (S/N)? ").upper().strip()
+        )
+
+        if comeu == "S":
             while True:
                 try:
-                    qtd_alimentos = int(input('Quantos alimentos o macaco comeu? '))
+                    qtd_alimentos = int(input("Quantos alimentos o macaco comeu? "))
                     break
-                
+
                 except ValueError:
-                    print('\n\033[mColoque um valor inteiro, quero saber a quantidade de alimentos que o macaco comeu\033[m\n')
-            
+                    print(
+                        "\n\033[mColoque um valor inteiro, quero saber a quantidade de alimentos que o macaco comeu\033[m\n"
+                    )
+
             for i in range(1, qtd_alimentos + 1):
-                alimentos.append(input(f'Qual foi o {i}° alimento do macaco? '))
-            
+                alimentos.append(input(f"Qual foi o {i}° alimento do macaco? "))
+
             bucho = alimentos
             return bucho
 
-
-        elif comeu == 'N':
+        elif comeu == "N":
             return alimentos
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     nome = pede_nome()
-    bucho =  pede_bucho(nome)
+    bucho = pede_bucho(nome)
     macaco1 = Macaco(nome, bucho)
     # menu
     while True:
         opcoes = [1, 2, 3, 4]
-        print('----Menu----\n')
+        print("----Menu----\n")
         try:
-            opcao = int(input('[1] Digerir\n'
-                '[2] Comer\n'
-                '[3] Ver bucho\n'
-                '[4] Sair\n\n'
-                'O que deseja? '))
-            
+            opcao = int(
+                input(
+                    "[1] Digerir\n[2] Comer\n[3] Ver bucho\n[4] Sair\n\nO que deseja? "
+                )
+            )
+
             if opcao not in opcoes:
-                print('\033[m\nPor favor coloque um opção válida!\n')
-            
+                print("\033[m\nPor favor coloque um opção válida!\n")
+
             else:
                 if opcao == 1:
                     macaco1.digerir()
-                
+
                 elif opcao == 2:
                     macaco1.comer()
-                
+
                 elif opcao == 3:
                     macaco1.ver_bucho()
-                
+
                 else:
                     break
-        
+
         except ValueError:
-            print('\nPor favor coloque um número inteiro!\n')
-        
+            print("\nPor favor coloque um número inteiro!\n")
+
         except Exception as e:
-            print(f'\nErro inesperado: {e.__class__.__name__}')
-    
-    print('\nObrigado e volte sempre!')
+            print(f"\nErro inesperado: {e.__class__.__name__}")
+
+    print("\nObrigado e volte sempre!")
