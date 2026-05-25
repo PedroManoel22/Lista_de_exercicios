725c00ab80f3d64f06a64d98aa3bb387b6fb3ffb
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-05-25 12:45:22 -0300

    Implementando Type Hints

--- sobre_classes/ex_04.py
+++ sobre_classes/ex_04.py
@@ -1,7 +1,7 @@
 # Crie uma classe que modele uma pessoa:
 
 # Atributos: nome, idade, peso e altura
-# Métodos: Envelhercer, engordar, emagrecer, crescer. Obs: Por padrão, 
+# Métodos: Envelhercer, engordar, emagrecer, crescer. Obs: Por padrão,
 # a cada ano que nossa pessoa envelhece, sendo a idade dela menor que 21 anos, ela deve crescer 0,5 cm.
 
 
@@ -12,65 +12,55 @@ class Pessoa:
         self._peso = peso
         self._altura = altura
 
-
     @property
     def idade(self):
         return self._idade
 
-
     @idade.setter
     def idade(self):
         self._idade
-    
-    
+
     @property
     def altura(self):
         return self._altura
-
 
     @altura.setter
     def altura(self):
         self._altura
 
-    
     @property
     def peso(self):
         return self._peso
 
-
     @peso.setter
     def peso(self):
         self._peso
-    
 
     def envelhecer(self):
         self._idade += 1
-        print(f'\nAgora {self.nome} tem {self._idade} anos!') 
+        print(f"\nAgora {self.nome} tem {self._idade} anos!")
 
         if self._idade < 21:
             self.crescer(0.5)
-        
-    
-    def engordar(self):
+
+    def engordar(self) -> None:
         self._peso += 1
-        print(f'\n{self.nome} engordou mais 1KG')
-        print(f'E agora está com {self._peso:.2f}KG')
-    
+        print(f"\n{self.nome} engordou mais 1KG")
+        print(f"E agora está com {self._peso:.2f}KG")
 
-    def emagrecer(self):
+    def emagrecer(self) -> None:
         self._peso -= 1
-        print(f'\n{self.nome} emagreceu  1KG')
-        print(f'E agora está com {self._peso:.2f}KG')
+        print(f"\n{self.nome} emagreceu  1KG")
+        print(f"E agora está com {self._peso:.2f}KG")
 
-    
     def crescer(self, aumenta: float = 0.2) -> None:
         self._altura += aumenta
 
-        print(f'\n{self.nome} cresceu mais {aumenta}cm')
-        print(f'Agora {self.nome} tem {self._altura}m')
+        print(f"\n{self.nome} cresceu mais {aumenta}cm")
+        print(f"Agora {self.nome} tem {self._altura}m")
 
 
-pessoa1 = Pessoa('Pedro', 21, 73, 1.73)
+pessoa1 = Pessoa("Pedro", 21, 73, 1.73)
 pessoa1.envelhecer()
 pessoa1.crescer()
 pessoa1.engordar()
