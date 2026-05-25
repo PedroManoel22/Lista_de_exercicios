d220c4a57f8b5c90205855f5956a9feff0b5b95f
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-05-25 12:44:53 -0300

    Implementando Type hints

--- sobre_classes/ex_01.py
+++ sobre_classes/ex_01.py
@@ -3,6 +3,7 @@
 # Atributos: cor, circunferência, material
 # Métodos: troca_cor e mostra_cor
 
+
 class Bola:
     def __init__(self, cor: str, circunferencia: str, material: str):
         self._cor = cor
@@ -12,17 +13,18 @@ class Bola:
     @property
     def cor(self):
         return self._cor
-    
+
     @cor.setter
     def cor(self, nova_cor: str):
         self._cor = nova_cor
-        print('A bola mudou de cor!')
-        
-    def mostra_cor(self):
-        print(f'A cor da bola de material de {self.material} é da cor {self.cor}')
+        print("A bola mudou de cor!")
+
+    def mostra_cor(self) -> None:
+        print(f"A cor da bola de material de {self.material} é da cor {self.cor}")
 
 
-bola1 = Bola('Azul', '25', 'borracha')
-bola1.mostra_cor()
-bola1.cor = 'Preta'
-bola1.mostra_cor()
+if __name__ == "__main__":
+    bola1 = Bola("Azul", "25", "borracha")
+    bola1.mostra_cor()
+    bola1.cor = "Preta"
+    bola1.mostra_cor()
