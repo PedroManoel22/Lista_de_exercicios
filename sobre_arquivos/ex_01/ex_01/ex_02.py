eb9497ad8f63a128e518f616a99c91b56dd6c654
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-05-25 12:41:31 -0300

    Implementando Type hints

--- sobre_arquivos/ex_01/ex_01/ex_02.py
+++ sobre_arquivos/ex_01/ex_01/ex_02.py
@@ -1,4 +1,4 @@
-# A ACME Inc., uma empresa de 500 funcionários, está tendo problemas de espaço em disco no seu servidor de arquivos. Para tentar resolver este problema, 
+# A ACME Inc., uma empresa de 500 funcionários, está tendo problemas de espaço em disco no seu servidor de arquivos. Para tentar resolver este problema,
 # o Administrador de Rede precisa saber qual o espaço ocupado pelos usuários, e identificar os usuários com maior espaço ocupado. Através de um programa,
 #  baixado da Internet, ele conseguiu gerar o seguinte arquivo, chamado "usuarios.txt":
 
@@ -9,7 +9,7 @@
 # cesar           987458
 # rosemary        789456125
 
-# Neste arquivo, o nome do usuário possui 15 caracteres. A partir deste arquivo, você deve criar um programa que gere um relatório, 
+# Neste arquivo, o nome do usuário possui 15 caracteres. A partir deste arquivo, você deve criar um programa que gere um relatório,
 # chamado "relatório.txt", no seguinte formato:
 
 # ACME Inc.               Uso do espaço em disco pelos usuários
@@ -27,32 +27,30 @@
 # Espaço médio ocupado: 430,26 MB
 
 
-
 def ler_arquivo() -> dict[str, str]:
     nome_arquivo = "Lista_de_exercicios/sobre_arquivos/ex_01/ex_01/usuarios.txt"
     dados_usuarios: dict[str, str] = {}
 
     try:
-        with open(nome_arquivo, 'r', encoding='utf-8') as arquivo:
-            
+        with open(nome_arquivo, "r", encoding="utf-8") as arquivo:
             for _, linha in enumerate(arquivo):
                 partes = linha.strip().split()
-                
+
                 if len(partes) == 2:
                     nome = partes[0]
                     numero = partes[1]
-                    
+
                     dados_usuarios[nome] = numero
 
     except FileNotFoundError:
         print(f"Erro: O arquivo \033[1;31m'{nome_arquivo}'\033[m não foi encontrado.")
     except Exception as e:
         print(f"Ocorreu um erro inesperado: {e}")
-    
+
     return dados_usuarios
 
 
-def gerar_relatorio(dados_usuarios: dict[str, str]):
+def gerar_relatorio(dados_usuarios: dict[str, str]) -> None:
 
     endereco_arquivo = "Lista_de_exercicios/sobre_arquivos/ex_01/ex_01/relatório.txt"
 
@@ -62,21 +60,21 @@ ACME Inc.               Uso do espaço em disco pelos usuários
 Nr.  Usuário        Espaço utilizado     % do uso
     """
 
-    LARGURA_NOME = 15   
-    LARGURA_MB = 10         
+    LARGURA_NOME = 15
+    LARGURA_MB = 10
     LARGURA_PCT = 16
     espaco_total = 0
     qtd_usuarios = 0
 
     try:
-        with open(endereco_arquivo, 'w', encoding='utf-8') as arquivo:
+        with open(endereco_arquivo, "w", encoding="utf-8") as arquivo:
             arquivo.write(cabecalho)
-            arquivo.write('\n')
+            arquivo.write("\n")
 
             # contabiliza o total de memória usada
             for _, nome in enumerate(dados_usuarios):
                 espaco_total += int(dados_usuarios[nome])
-                
+
             espaco_total = round(espaco_total / (1024 * 1024), 2)
             espaco_total_str = str(espaco_total)
 
@@ -90,23 +88,25 @@ Nr.  Usuário        Espaço utilizado     % do uso
                 porcentagem = round((espaco * 100) / espaco_total, 2)
 
                 arquivo.write(
-                    f"{indice_str} "  
-                    f"{nome:<{LARGURA_NOME}} " 
+                    f"{indice_str} "
+                    f"{nome:<{LARGURA_NOME}} "
                     f"{espaco_str:>{LARGURA_MB}} MB"
                     f"{porcentagem:>{LARGURA_PCT}}%\n"
-                             )
-            arquivo.write(f'\nEspaço total ocupado: {espaco_total_str} MB')
+                )
+            arquivo.write(f"\nEspaço total ocupado: {espaco_total_str} MB")
             qtd_usuarios = len(dados_usuarios)
             espaco_medio = round(espaco_total / qtd_usuarios, 2)
-            arquivo.write(f'\nEspaço médio ocupado: {espaco_medio} MB')
-            
-            print(f"Arquivo '\033[1;32m{endereco_arquivo}\033[m' criado e escrito com sucesso.")
+            arquivo.write(f"\nEspaço médio ocupado: {espaco_medio} MB")
+
+            print(
+                f"Arquivo '\033[1;32m{endereco_arquivo}\033[m' criado e escrito com sucesso."
+            )
 
     except IOError as e:
         print(f"Erro ao manipular o arquivo: {e}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     ...
     oi = ler_arquivo()
     print(oi)
