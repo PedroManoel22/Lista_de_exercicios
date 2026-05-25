16be1c4f39b75285b592a39de64b515a0a38051a
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-05-25 12:45:07 -0300

    Implementando Type Hints

--- sobre_classes/ex_02.py
+++ sobre_classes/ex_02.py
@@ -4,33 +4,31 @@
 # Atributos: Tamanho do lado
 # Métodos: Mudar valor do Lado, Retornar valor do Lado e calcular Área;
 
+
 class Quadrado:
     def __init__(self, lado: float):
         self._lado = lado
-    
+
     @property
     def lado(self):
         return self._lado
-    
 
     @lado.setter
     def lado(self, valor: float):
         self._lado = valor
-    
 
     def retona_valor_lado(self):
-        print(f'O valor do lado é de {self._lado}')
-    
+        print(f"O valor do lado é de {self._lado}")
 
     def calcular_area(self):
         area = self._lado * self._lado
-        print(f'A área do quadrado de lado {self._lado} = {area}m²')
+        print(f"A área do quadrado de lado {self._lado} = {area}m²")
 
 
-quadrado1 = Quadrado(25)
-quadrado1.retona_valor_lado()
-quadrado1.calcular_area()
-quadrado1.lado = 5
-quadrado1.retona_valor_lado()
-quadrado1.calcular_area()
-
+if __name__ == "__main__":
+    quadrado1 = Quadrado(25)
+    quadrado1.retona_valor_lado()
+    quadrado1.calcular_area()
+    quadrado1.lado = 5
+    quadrado1.retona_valor_lado()
+    quadrado1.calcular_area()
