cf18af9097d6d6b0d8ec3ca8250c3970720da468
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-06-04 09:24:24 -0300

    Implementando type hintsd

--- sobre_listas/ex_05.py
+++ sobre_listas/ex_05.py
@@ -1,55 +1,45 @@
-# Faça um programa que leia 20 números inteiros e armazene-os num vetor. 
+# Faça um programa que leia 20 números inteiros e armazene-os num vetor.
 # Armazene os números pares no vetor PAR e os números IMPARES no vetor impar. Imprima os três vetores
+
 
 def valida_coleta_int(ordem: int) -> list[int]:
 
-    global numeros
     numeros: list[int] = []
 
     for i in range(1, ordem + 1):
         while True:
-
             try:
-
-                num = input(f'Insira o {i}° número: ')
+                num = input(f"Insira o {i}° número: ")
                 num_int = int(num)
                 numeros.append(num_int)
-                
+
                 break
 
             except ValueError:
-
-                print('\033[1;31mPor favor insira um número inteiro!\033[m')
+                print("\033[1;31mPor favor insira um número inteiro!\033[m")
 
     return numeros
 
 
 def pares_impares(nums: list[int]) -> tuple[list[int], list[int]]:
-
-    global pares
-    global impares
     pares: list[int] = []
     impares: list[int] = []
 
     for num in nums:
-
         if num % 2 == 0:
-
             pares.append(num)
-        
-        else:
 
+        else:
             impares.append(num)
 
     return pares, impares
 
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     QUANTIDADE_NUMEROS = 20
 
-    valida_coleta_int(QUANTIDADE_NUMEROS)
-    pares_impares(numeros)
-    print(f'Números: {numeros}')
-    print(f'Pares: {pares}')
-    print(f'Ímpares: {impares}')
+    numeros = valida_coleta_int(QUANTIDADE_NUMEROS)
+    pares, impares = pares_impares(numeros)
+    print(f"Números: {numeros}")
+    print(f"Pares: {pares}")
+    print(f"Ímpares: {impares}")
