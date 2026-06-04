720f033fed4f406f24c0452c289625cf267b9539
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-06-04 09:23:47 -0300

    Implementando type hintsd

--- sobre_listas/ex_01.py
+++ sobre_listas/ex_01.py
@@ -10,25 +10,22 @@ def coletar_numeros(quantidade: int) -> list[int]:
     return numeros
 
 
-def validar_num(x: int):
+def validar_num(x: int) -> int:
 
     while True:
         try:
-            num = input(f'Insira o {x}° número inteiro: ')
+            num = input(f"Insira o {x}° número inteiro: ")
             num_int = int(num)
             break
         except ValueError:
-            print('\033[1;31mColoque um número inteiro!\033[m')
-        
+            print("\033[1;31mColoque um número inteiro!\033[m")
+
     return num_int
 
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     NUMEROS_PARA_COLETAR = 5
-    
-    numeros_inseridos = coletar_numeros(NUMEROS_PARA_COLETAR)
-    
-    print(f'Números inseridos: {numeros_inseridos}')
 
-        
+    numeros_inseridos = coletar_numeros(NUMEROS_PARA_COLETAR)
+
+    print(f"Números inseridos: {numeros_inseridos}")
