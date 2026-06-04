a98c35f717d28f097be25c7cec29b8895ae5b571
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-06-04 09:24:12 -0300

    Implementando type hintsd

--- sobre_listas/ex_03.py
+++ sobre_listas/ex_03.py
@@ -1,44 +1,39 @@
 # Faça um programa que leia 4 notas, mostre as notas e a média na tela.
 
-def validar_notas(ordem: float):
+
+def validar_notas(ordem: float) -> float:
 
     while True:
-
         try:
-
-            nota = input(f'Insira a {ordem}° nota: ')
+            nota = input(f"Insira a {ordem}° nota: ")
             nota_int_float = float(nota) or int(nota)
 
             if nota_int_float > 10 or nota_int_float < 0:
-
-                print('\033[1;33mPor favor coloque um nota entre 0 a 10!\033[m')
+                print("\033[1;33mPor favor coloque um nota entre 0 a 10!\033[m")
                 continue
 
             return nota_int_float
 
         except ValueError:
+            print("\033[1;31mPor favor coloque uma nota válida\033[m")
 
-            print('\033[1;31mPor favor coloque uma nota válida\033[m')
 
-def coletar_notas(quantidade: int) -> list[float]: 
+def coletar_notas(quantidade: int) -> list[float]:
 
-    global notas    
     notas: list[float] = []
 
     for i in range(1, quantidade + 1):
-
         notas.append(validar_notas(i))
 
     return notas
 
-if __name__  == '__main__':
 
-    QUANTIDADE_NOTAS = 4 # variavel constante 
+if __name__ == "__main__":
+    QUANTIDADE_NOTAS = 4  # variavel constante
 
     notas_coletadas = coletar_notas(QUANTIDADE_NOTAS)
 
-    print(f'Notas: {notas_coletadas}')
-    media = sum(notas) / QUANTIDADE_NOTAS
+    print(f"Notas: {notas_coletadas}")
+    media = sum(notas_coletadas) / QUANTIDADE_NOTAS
 
-    print(f'Média: {media:.2f}') 
-        
+    print(f"Média: {media:.2f}")
