59c59092cadbaeecd957eeea036ce9a73ba13480
Pedro Manoel <pedromanoelpyjs@gmail.com>
2026-06-10 09:47:31 -0300

    Reformulando código conforme PEP 8

--- sobre_strings/ex_11/ex_11.py
+++ sobre_strings/ex_11/ex_11.py
@@ -15,11 +15,12 @@
 
 from random import randint
 
+
 def ler_arquivo() -> list[str]:
-    caminho_arquivo = 'Lista_de_exercicios/sobre_strings.py/ex_11/palavras.txt'
-    with open(caminho_arquivo, 'r') as arquivo:
+    caminho_arquivo = "Lista_de_exercicios/sobre_strings.py/ex_11/palavras.txt"
+    with open(caminho_arquivo, "r") as arquivo:
         palavras = arquivo.read().split()
-    
+
     return palavras
 
 
@@ -32,77 +33,76 @@ def escolhe_palavra(palavras: list[str]) -> str:
 
 def comecar_jogo(palavra_escolhida: str) -> None:
     tamanho_palavra: int = len(palavra_escolhida)
-    palavra_escolhida  = palavra_escolhida.lower()
-    caracteres: list[str] =  ['_ '] * tamanho_palavra
-    palavra_formada = ''
+    palavra_escolhida = palavra_escolhida.lower()
+    caracteres: list[str] = ["_ "] * tamanho_palavra
+    palavra_formada = ""
     cont: int = 0
     letras_ja_escolhidas: list[str] = []
 
-
     while True:
-        letra = input('Digite uma letra: ').lower()
+        letra = input("Digite uma letra: ").lower()
 
         # verifica se a letra escolhida é repetida
         if letra not in letras_ja_escolhidas:
             letras_ja_escolhidas.append(letra)
         else:
             print()
-            print(f'\033[1;36m{letra} já foi escolhida, tente outra\033[m')
+            print(f"\033[1;36m{letra} já foi escolhida, tente outra\033[m")
             print()
             continue
 
         # verifica se a variável "letra" tem mais de um caractere
         if len(letra) > 1:
             print()
-            print('\033[1;31mPor favor insira um letra de cada vez!\033[m')
+            print("\033[1;31mPor favor insira um letra de cada vez!\033[m")
             print()
             continue
 
         # verifica se a variável "letra" é um número
         elif letra.isdigit():
             print()
-            print('\033[1;31mInsira uma letra e não número\033[m')
+            print("\033[1;31mInsira uma letra e não número\033[m")
             print()
             continue
-        
-        #verifica se a variável "letra" está vazia
+
+        # verifica se a variável "letra" está vazia
         elif not letra:
             print()
-            print('\033[1;31mInsira uma letra!\033[m')
+            print("\033[1;31mInsira uma letra!\033[m")
             print()
             continue
-        
+
         # verifica se a letra que o usuário digitou está na palavra_escolhida
         elif letra in palavra_escolhida:
-            print('A palavra é: ', end='')
+            print("A palavra é: ", end="")
 
             for i in range(tamanho_palavra):
                 if letra == palavra_escolhida[i]:
                     caracteres[i] = letra
                     palavra_formada += letra
             for caractere in caracteres:
-                print(f'{caractere}', end='')
-            
+                print(f"{caractere}", end="")
+
             print()
 
             if sorted(palavra_formada) == sorted(palavra_escolhida):
                 print()
-                print('\033[1;32mParabéns você ganhou\033[m')
+                print("\033[1;32mParabéns você ganhou\033[m")
                 print()
                 break
-        
-        # caso o usuário não acerte a letra 
+
+        # caso o usuário não acerte a letra
         else:
             cont += 1
-            print(f'\033[1;31mVocê errou pela {cont}ª vez, tente novamente!\033[m')
+            print(f"\033[1;31mVocê errou pela {cont}ª vez, tente novamente!\033[m")
             if cont == 6:
                 print()
-                print('\033[1;31mVocê perdeu!\033[m')
+                print("\033[1;31mVocê perdeu!\033[m")
                 print()
                 break
-            
-                      
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     ler_palavras = ler_arquivo()
     palavra = escolhe_palavra(ler_palavras)
     comecar_jogo(palavra)
--- sobre_strings/ex_11/teste.py
+++ sobre_strings/ex_11/teste.py
@@ -1,7 +1,6 @@
-
-x = 'cama'
-y = 'aacm'
+x = "cama"
+y = "aacm"
 print(sorted(y))
 print(sorted(x))
 if sorted(y) == sorted(x):
-    print('hehe')
+    print("hehe")
